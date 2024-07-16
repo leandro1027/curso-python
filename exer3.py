e7dd119b06c38764dbd5d1e91cd7fbfd1b07066d
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2024-07-16 15:54:44 -0300

    Update exer3.py

--- exer3.py
+++ exer3.py
@@ -1,1 +1,14 @@
-s
+"""
+Exercício
+Exiba os índices da lista
+0 Maria
+1 Helena
+2 Luiz
+"""
+
+lista = ['Maria', 'Helena', 'Luiz']
+
+indices = range(len(lista))
+
+for indice in indices:
+    print(indice, lista[indice])
