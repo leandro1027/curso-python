98ffb736f449adaa75a4aaed669e852dce5653d1
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2024-07-16 11:55:27 -0300

    Update aula8.py

--- aula8.py
+++ aula8.py
@@ -1,2 +1,32 @@
-print("teste")
-print('ola')
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+"""
+
+# lista = ['leandro', 123, True]
+
+# print(lista[0])
+# lista[0] = 'teste'
+# print(lista[0])
+
+# lista2 = [1,2,3]
+# numero = lista2[1]
+# print(numero)
+
+# print(lista2)
+# del lista2[0] #del exclue
+# print(lista2)
+# lista2.append(4) #append adiciona elemento a lista
+# print(lista2)
+
+while True:
+    add_item_na_lista = input("Adicione algo na lista: ")
+    lista3 = []
+
+    lista3.append(add_item_na_lista)
+   
+    print(lista3)
+    lista3 +=1
