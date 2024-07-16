593bd77efab1d85fd37cd35d31def9324193bc6f
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2024-07-16 16:21:35 -0300

    Update aula9.py

--- aula9.py
+++ aula9.py
@@ -1,6 +1,12 @@
 #desempacomento 
 
-nome1, _, *resto = ['leandro', 'Balaban', 'teste', 'teste1']
-print(nome1, _, resto)
+# nome1, _, *resto = ['leandro', 'Balaban', 'teste', 'teste1']
+# print(nome1, _, resto)
 
 # tuplas
+
+nomes = 'leandro', 'Balaban', 'teste'
+
+print(nomes[0])
+for nome in nomes:
+    print(nome)
