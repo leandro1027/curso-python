182f66d09cc5ca5810ef24fccd39387fea1f0c1a
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2024-07-17 10:14:51 -0300

    Update exer4.py

--- exer4.py
+++ exer4.py
@@ -5,3 +5,44 @@ inserir, apagar e listar valores da sua lista
 Não permita que o programa quebre com 
 erros de índices inexistentes na lista.
 """
+import os
+
+lista = []
+while True:
+    print("Escolha uma opção: ")
+    opcao = input("[I]nserir [a]pagar [l]istar: ")
+
+    if opcao == 'i':
+        os.system('cls')
+        valor = input("Digite o item: ")
+        lista.append(valor)
+    elif opcao == 'a':
+        print('a')
+        indice_str = (input("Digite o indice que deseja apagar: "))
+    
+        try:
+            indice = int(indice_str)
+            del lista[indice]
+        except ValueError:
+                print('Por favor digite número int.')
+        except IndexError:
+                print('Índice não existe na lista')
+        except Exception:
+                print('Erro desconhecido')
+
+    elif opcao == 'l': 
+        print('l')
+        os.system('cls')
+
+        if len(lista) == 0:
+            print("Nada a mostrar, lista vazia")
+        
+        for i, valor in enumerate(lista):
+             print(i, valor)
+
+    else:
+        print("Por favor escolha alguma das opcôes!")
+    
+
+       
+             
