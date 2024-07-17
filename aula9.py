328266d2d10598ee9fd5113316391827ade53bc3
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2024-07-17 09:16:04 -0300

    Update aula9.py

--- aula9.py
+++ aula9.py
@@ -5,8 +5,16 @@
 
 # tuplas
 
-nomes = 'leandro', 'Balaban', 'teste'
+# nomes = 'leandro', 'Balaban', 'teste'
 
-print(nomes[0])
-for nome in nomes:
-    print(nome)
+# print(nomes[0])
+# for nome in nomes:
+#     print(nome)
+
+# para enumerar itens da tupla, usa-se enumerate()
+lista = ["leandro", "Balaban"]
+
+lista_enumerada = enumerate(lista)
+
+for item in lista_enumerada:
+    print(item)
