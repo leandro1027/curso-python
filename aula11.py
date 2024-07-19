305a2948837cd20ad5e496ed308ad637675ba5c7
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2024-07-19 09:16:04 -0300

    Update aula11.py

--- aula11.py
+++ aula11.py
@@ -3,7 +3,14 @@ split e join com list e str
 split - divide uma string (list)
 join - une uma string
 """
-frase = 'fazendo apenas um teste'
+frase = '   Olha só que   , coisa interessante          '
+lista_frases_cruas = frase.split(',')
 
-lista_palavras = frase.split()
-print(lista_palavras)
+lista_frases = []
+for i, frase in enumerate(lista_frases_cruas):
+    lista_frases.append(lista_frases_cruas[i].strip())
+
+# print(lista_frases_cruas)
+# print(lista_frases)
+frases_unidas = ', '.join(lista_frases)
+print(frases_unidas)
